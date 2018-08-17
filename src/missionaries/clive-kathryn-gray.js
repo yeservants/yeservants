242ad9bb22be4ce5451015d3799344641f628c07
Tony <1414927+zfbTony@users.noreply.github.com>
2018-08-17 00:38:51 -0400

    Clive changes

--- src/missionaries/clive-kathryn-gray.js
+++ src/missionaries/clive-kathryn-gray.js
@@ -4,11 +4,11 @@ exports.info = {
     location: "Northern Ireland, UK",
     picture: "clive-kathryn-gray.jpg",
     contact: {
-        "address": [
-            "18 Farm Lodge Stables",
-            "Greenisland  BT38 8ZL", 
-            "Northern Ireland, UK"
-        ],
+        //"address": [
+        //    "18 Farm Lodge Stables",
+        //    "Greenisland  BT38 8ZL", 
+        //    "Northern Ireland, UK"
+        //],
         "phone": "+44 7496 379 192",
         "email": "clivegray1@gmail.com"
     },
@@ -22,7 +22,12 @@ return `
 <b>Duration:</b> Full-Time
 
 <b>About</b>
-Clive grew up in Ghana, West Africa and followed his dream to become a pilot, while Kathryn grew up in Florida, USA and became an ESL Teacher. Together with our three (now adult) children, we served in Indonesia from 2005-2015 in Missionary Aviation and English Education.  With Clive having retired from flying in 2015 we are now based in Northern Ireland, UK where we are both pursuing Master’s Degrees in Transformational Leadership.  We are also embarking upon an exciting new partnership with Simply The Story as global Instructors for STS Oral Bible Story Workshops <a href="http://www.simplythestory.org" target="_blank" rel="nofollow">simplythestory.org</a>.  Did you know that over 80% of the world’s people either cannot read, or prefer not to learn from reading?  Yet they need to receive the Gospel in a package that they can open, understand and pass on to others.  We have personally been transformed after encountering the Biblical narrative in its original oral format.  Now, we want to share our experience to help others develop the skills necessary to study the Bible accurately and inductively, asking purposeful questions that encourage men, women and children around the world to discover spiritual truths.
+Clive grew up in Ghana, West Africa the son of Bible translators and followed his dream to become a pilot, while Kathryn grew up in Florida, USA and became an ESL (English as a Second Language), teacher. Together with our three (now adult) children, we served in Indonesia from 2005-2015 in Missionary Aviation and English Education.  Clive and Kathryn finished their Master's degrees in Transformational Leadership in 2018 and Clive as a Spiritual Director.  We have personally been transformed after encountering the Biblical narrative in its original oral format.  Now, we want to share our experience to help others develop the skills necessary to study the Bible accurately and inductively, asking purposeful questions that encourage men, women and children around the world to discover spiritual truths.  Our Father God has filled our hearts with a deeper longing to take his message of Joy and Peace to the broken, the poor, the blind and the crippled.  In 2018 we return to the lovely people of Indonesia and take this the greatest invitation to join the Masters' banquet (Luke 14:15-25).
+
+Pray our eyes be opened to see and hear things we could never have imagined or hoped for.  Pray that the kingdoms and principalities of darkness are pushed back, and the Kingdom of God be advanced in the great name of Jesus…one heart at a time.
+
+The Masters Servants,
+Clive and Kathryn Gray
 `;
 };
 
