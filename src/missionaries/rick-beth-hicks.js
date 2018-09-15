7d39789ab549053cdfe7b9b9bfcf5671361756e5
Tony <1414927+zfbTony@users.noreply.github.com>
2018-09-15 19:37:41 -0400

    New missionary

---
+++ src/missionaries/rick-beth-hicks.js
@@ -0,0 +1,33 @@
+exports.info = {
+    name: "Rick and Beth Hicks", 
+    url: "hicks",
+    location: "Ozamiz, Philippines",
+    picture: "rick-beth-hicks.png",
+    contact: { 
+        "none": "none"
+    },
+    description: "Interesting stuff about me is I wrote this website."
+}
+
+exports.bio = () => { 
+return `
+<b>Sending Church:</b> Unknown
+<b>Ministry Started:</b> 2002
+<b>Duration:</b> Full-Time
+
+<b>About</b>
+Rick and Beth have been married 37 years. They have 2 adult children and one granddaughter. For 10 years the Hicks attended Celebration Community Church and served in this community. They had both been involved in short term missions , which was the beginning of God working in their hearts to one day being full-time on the mission field. In 2012 they sold their home in Celebration Florida and relocated to Ozamiz City, Philippines. They live and serve in Ozamiz City.
+
+<b>Ministry</b>
+What kind of world awaits our children if at home looms the cruelty of child abuse, malnutrition, incestuous rapes, and poverty that takes every bit of decency from a human life. We believe that mercy and love are crucial in letting the world know that God is the father to the fatherless. That is why we are dedicated to bringing the love of Christ to those around us and reaching the community in meaningful and lasting ways. Through addressing the immediate physical needs of an individual, we can then focus on their spiritual needs-sharing the Gospel and transforming their life with the Good news.
+
+<b>Personal Message</b>
+We thank God for those that partner with us through, prayer, love and financial support. You are our partners and we could not do this work without you. Our hearts are continually overwhelmed by God’s faithfulness. Our prayer is that He will bless each of you abundantly.
+`;
+};
+
+exports.bio_es = () => { 
+return `
+`;
+};
+
