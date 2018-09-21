8110732d300dbf4da41aeffb6ed6b064d57501cb
Tony <1414927+zfbTony@users.noreply.github.com>
2018-09-21 01:56:47 -0400

    new missionary

---
+++ src/missionaries/joe-heike-batluck.js
@@ -0,0 +1,27 @@
+exports.info = {
+    name: "Joe and Jeike Batluck", 
+    url: "batluck",
+    location: "Germany",
+    picture: "none.jpg",
+    contact: {
+        "none": "none"
+    },
+    description: "The Batlucks live in Kandern, Germany, where Joe serves as lead pastor of Black Forest Christian Fellowship."
+}
+
+exports.bio = () => {
+return `
+<b>Sending Church:</b> Trinity Bible Church, Cypress, TX, USA
+<b>Ministry Started:</b> 2018
+<b>Duration:</b> Full-Time
+
+<b>About</b>
+The Batlucks live in Kandern, Germany, where Joe serves as lead pastor of Black Forest Christian Fellowship. As a church comprised primarily of missionaries from a various missions such as GEM, SIL/Wycliff, Teach Beyond, IMB, CMA, TEAM, Young Life and many others and missionary kids who are attending Black Forest Academy, BFCF plays a strategic role in equipping these saints for the gospel work that they are doing not only in Germany but through Europe, Africa and the Middle East.
+`;
+};
+
+exports.bio_es = () => {
+return `
+`;
+};
+
