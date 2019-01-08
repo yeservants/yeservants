bfd80644cd6b3ba336e1e726a3324c2287b5f4ef
Tony <1414927+zfbTony@users.noreply.github.com>
2019-01-08 04:05:16 -0500

    updates to burgess and copyright update

--- src/missionaries/william-chantell-burgess.js
+++ src/missionaries/william-chantell-burgess.js
@@ -1,7 +1,7 @@
 exports.info = {
     name: "William and Chantell Burgess",
     url: "william-and-chantell-burgess",
-    location: "Belize, Central America",
+    location: "Zambia, Southern Africa",
     picture: "burgess.jpg",
     contact: { 
         "email": "willandchantell@gmail.com"
@@ -11,14 +11,13 @@ exports.info = {
 
 exports.bio = () => { 
 return `
-<b>Sending Church:</b> First Church of City
-<b>Ministry Started:</b> September 2004
+<b>Ministry Started:</b> September 2001
 <b>Duration:</b> Full-Time
 
 <b>About</b>
-William and Chantell Burgess have been missionaries since 2001 and have been married for 19yrs.  They are both certified trainers of the Leadership Challenge Association, one of the world's most recognized leadership training development organizations. They have served as missionaries in Zambia, Southern Africa. While in Zambia, they worked with World Vision in rural villages doing humanitarian work, building schools, providing school materials for children, and helping provide clean water. They have also done mission work in Namibia, Africa, and Central America.
+William and Chantell Burgess have been missionaries since 2001 and have been married for 23yrs. They are both certified trainers of the Leadership Challenge Association, one of the world's most recognized leadership training development organizations. 
 
-Currently they are in Belize, Central America. Unlike the frontier mission work they did in Zambia, Belize has a tremendous need for discipleship, leadership training and evangelism.  They are located in a very strategic place with distressed areas all around and they are right in the middle of them.  This gives them lots of opportunities for relational evangelism in the area..
+They have served as missionaries in Zambia, Southern Africa. While in Zambia their work entailed equipping the locals through discipleship, relational evangelism and leadership training. They have also helped with church planting, providing clean water and outreaches showing the Jesus film. They have done mission work in Namibia, Africa, and Central America.
 `;
 };
 
