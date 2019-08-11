93d3b0f33f427d461f71bc9aa5b7b0a5b219605c
Tony <1414927+zfbTony@users.noreply.github.com>
2019-08-11 14:10:10 -0400

    added paul-timothy

---
+++ src/missionaries/paul-timothy-deborah.js
@@ -0,0 +1,31 @@
+exports.info = {
+    name: "Paul-Timothy & Deborah",
+    url: "paul-timothy", 
+    location: "East Asia",
+    picture: "none.jpg", 
+    contact: { 
+        "none": "none"
+    },
+    description: "The information on Paul Timothy and family are undisclosed as they work in a closed country. Please pray for them as they seek to bring His kingdom."
+}
+
+exports.bio = () => { 
+return `
+<b>Sending Church:</b> New Hope Community
+<b>Ministry Started:</b> 2008
+<b>Duration:</b> Full-Time
+
+<b>Status:</b> 60% funds raised
+
+<b>About</b>
+The information on Paul Timothy and family are undisclosed as they work in a closed country. Please pray for them as they seek to bring His kingdom.
+
+
+<b>Personal Message:</b> We are privileged to serve and we are grateful for all of our partners as we stand together to advance His kingdom. `;
+};
+
+exports.bio_es = () => { 
+return `
+`;
+};
+
