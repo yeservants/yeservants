4b55f5bc2a303248a7d1e533c5ac5acbe70d4477
Tony <1414927+zfbTony@users.noreply.github.com>
2019-10-10 21:39:49 -0400

    Added Kayla Underwood to missionaries

---
+++ src/missionaries/kayla-underwood.js
@@ -0,0 +1,24 @@
+exports.info = {
+    name: "Kayla Underwood",
+    url: "underwood",
+    location: "Ethiopia",
+    picture: "kayla_underwood.jpg", //Should this just be their [url].png (500x500px, 1:1 ratio, .jpeg preffered)
+    contact: {
+    },
+    description: "I will also be working alongside local churches who desire to serve families affected by disabilities.." //keep this short
+}
+
+exports.bio = () => { //This is where the main body goes written in the 1st person 
+return `
+<b>Duration:</b> Full-Time
+
+<b>About</b>
+I will be serving as a full-time missionary with Selamta Family Project in Addis Ababa, Ethiopia. Selamta Family Project creates forever families for orphaned and abandoned children & recently launched their second generation of Forever Family Homes, including 5 kids with special needs. I believe that people with special needs are an unreached people group due to the lack of understanding of the potential they have to learn and understand. I will train & educate the local staff and mothers regarding how to care for kids with special needs. I will work alongside the staff and families to determine what resources these children need to thrive, while working with local and overseas ministries to provide these resources. I plan to connect with local ministries and schools in hopes of creating a network to serve families who have kids with special needs. I will also be working alongside local churches who desire to serve families affected by disabilities to start special needs ministries. In this, I hope that the Gospel will be proclaimed in this area to those who have not heard before.
+
+`;
+};
+
+exports.bio_es = () => { //optional spanish version?
+return ``;
+};
+
