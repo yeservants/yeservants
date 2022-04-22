ce4b4cae9509d5ecbf192f0ee11b5c06a95ff7fd
Tony <1414927+zfbx@users.noreply.github.com>
2022-04-21 23:50:04 -0400

    update chuck

--- src/missionaries/chuck-ann-tompkins.js
+++ src/missionaries/chuck-ann-tompkins.js
@@ -5,13 +5,13 @@ exports.info = {
     picture: "tompkins.jpg",
     contact: {
         "address": [
-            "170 Muzzy Road",
+            "1344 Ellis Hollow Road",
             "Ithaca, NY 14850"
         ],
         "phone": "607-272-0044",
         "email": "chucktompkins@earthlink.net"
     },
-    description: "Chuck and Ann Tompkins plant the gospel and encourage the church in Ithaca, NY by working with students on the campuses of Cornell University and Ithaca College"
+    description: "Chuck and Ann Tompkins plant the gospel and encourage the church in Ithaca, NY by working with students on the campuses of Cornell University"
 }
 
 exports.bio = () => {
@@ -21,10 +21,10 @@ return `
 <b>Duration:</b> Full-Time
 
 <b>About</b>
-Chuck and Ann Tompkins plant the gospel and encourage the church in Ithaca, NY by working with students on the campuses of Cornell University and Ithaca College, serving international students, facilitating inter-church unity, networking with various organizations both locally and regionally and providing leadership in different venues.
+Chuck and Ann Tompkins plant the gospel and encourage the church in Ithaca, NY by working with students on the campuses of Cornell University, serving international students, facilitating inter-church unity, networking with various organizations both locally and regionally and providing leadership in different venues.
 
 <b>Ministry</b>
-We were in traditional pastoral ministry for over 34 years. We resigned from Bethel Grove Bible Church in Ithaca in February 2005 (after 20 years) to pursue a different calling. Passionate about bridging the gap between the gospel and community, we are intent on equipping and encouraging friends to live out the gospel among the lost, and to facilitate emerging small communities of faith that may gather anywhere or at any time. Our signature strength over the years has been to encourage others, prompting us to call ourselves The Barnabas Team--'encouraging others to get in the Way'. We work with students at Cornell University and Ithaca College, serve on various boards and with several organizations, while facilitating a home gathering. Chuck aspires to write and coaches persons online.
+We were in traditional pastoral ministry for over 34 years. We resigned from Bethel Grove Bible Church in Ithaca in February 2005 (after 20 years) to pursue a different calling. Passionate about bridging the gap between the gospel and community, we are intent on equipping and encouraging friends to live out the gospel among the lost, and to facilitate emerging small communities of faith that may gather anywhere or at any time. Our signature strength over the years has been to encourage others, prompting us to call ourselves The Barnabas Team--'encouraging others to get in the Way'. We work with students at Cornell University, serve on various boards and with several organizations, while facilitating a home gathering. Chuck aspires to write and coaches persons online.
 
 <b>Personal Message</b>
 God has been faithful to meet our needs through friends who affirm our ministry and emphases. We feel confident that God has led for us to remain in Ithaca, building on the relational capital of many years. Chuck works part time to meet financial needs, though his primary passion is working with people. We are grateful to the Lord for whoever is able to be part of our financial support team.
