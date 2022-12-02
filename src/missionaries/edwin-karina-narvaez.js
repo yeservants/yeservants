076213708445f35270ff85e21ff895757ab98c14
Tony zfbx <1414927+zfbx@users.noreply.github.com>
2022-12-02 16:57:33 -0500

    Added Narvaez

---
+++ src/missionaries/edwin-karina-narvaez.js
@@ -0,0 +1,27 @@
+exports.info = {
+    name: "Edwin and Karina Narvaez",
+    url: "narvaez",
+    location: "Carazo, Nicaragua",
+    picture: "narvaez.png",
+    contact: {
+        "none": ""
+    },
+    description: "Edwin and Karina Narvaez, born in Nicaragua and Mexico respectively, members of Greeneway Church in Orlando FL..."
+}
+
+exports.bio = () => {
+return `<b>About</b>
+Edwin and Karina Narvaez, born in Nicaragua and Mexico respectively, members of Greeneway Church in Orlando FL.
+In 2011 Edwin felt the to be a missionary to Nicaragua, his ministry consists of continually traveling to different regions of that country, bringing Christ's voice of hope, trying to touch the most needy, those who live in garbage dumps and very poor regions. , bringing them aid and food.
+Both have felt from the Lord Jesus Christ to dedicate themselves full time in the Kingdom of God. Their main mission is to carry the message of salvation and hope, help children in need by providing them with food, clothing, shoes, backpacks with school supplies and toys at Christmas time.
+Their vision is to build a place in the city of Carazo, Nicaragua where they can house more than 200 children and provide them with food, bilingual spiritual and secular education, sports, and for them to learn different skills such as carpentry, plumbing, welding, electricity, pastry, sewing and others, not leaving aside the training of abandoned mothers who fight for those children.
+
+Please, keep them in prayer so that they can achieve the plans that God has placed in their hearts, I know that together we can reach the children of Nicaragua.
+`;
+};
+
+/*
+<b>Sending Church:</b> 
+<b>Ministry Started:</b> 
+<b>Duration:</b> Full-Time
+*/
