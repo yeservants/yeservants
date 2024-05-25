d065957370cecd240110dc92d33f33fa87bef263
Daniel Campo <dce82@me.com>
2024-05-25 19:49:02 -0400

    scriptfor icon updated

--- docs/js/active.js
+++ docs/js/active.js
@@ -163,3 +163,14 @@
          delay: 300,
      });
  });
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+            var oldFacebookIcon = document.querySelector('.footer-charity-text .fa-facebook');
+
+            if (oldFacebookIcon) {
+                var newFacebookIcon = document.createElement('i');
+                newFacebookIcon.className = 'bi bi-facebook';
+                oldFacebookIcon.parentNode.replaceChild(newFacebookIcon, oldFacebookIcon);
+            }
+        });
