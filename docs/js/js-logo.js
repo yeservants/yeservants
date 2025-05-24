aa1e094708b1123f26355206d611b02a33ad170e
daniel campo <dce82@me.com>
2025-05-24 16:14:13 -0400

    logo cde4

--- docs/js/js-logo.js
+++ docs/js/js-logo.js
@@ -1,9 +1,18 @@
-    document.addEventListener("DOMContentLoaded", function() {
-            var oldFacebookIcon = document.querySelector('.footer-charity-text .fa-facebook');
+document.addEventListener("DOMContentLoaded", function () {
+  // Replace old Facebook icon
+  var oldFacebookIcon = document.querySelector('.footer-charity-text .fa-facebook');
+  if (oldFacebookIcon) {
+    var newFacebookIcon = document.createElement('i');
+    newFacebookIcon.className = 'bi bi-facebook';
+    oldFacebookIcon.parentNode.replaceChild(newFacebookIcon, oldFacebookIcon);
+  }
 
-            if (oldFacebookIcon) {
-                var newFacebookIcon = document.createElement('i');
-                newFacebookIcon.className = 'bi bi-facebook';
-                oldFacebookIcon.parentNode.replaceChild(newFacebookIcon, oldFacebookIcon);
-            }
-        });
+  // Add click redirect to image inside .main-logo
+  const logoImg = document.querySelector(".main-logo img");
+  if (logoImg) {
+    logoImg.style.cursor = "pointer"; // optional: make it look clickable
+    logoImg.addEventListener("click", () => {
+      window.location.href = "https://yeservants.org/";
+    });
+  }
+});
